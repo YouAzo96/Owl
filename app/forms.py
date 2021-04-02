108289ebbacc3c4ec2028c04ac5158022e8c3850
ElvisFern <78712154+ElvisFern@users.noreply.github.com>
2021-04-02 15:24:02 -0400

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -3,12 +3,12 @@ from wtforms import StringField, IntegerField, SubmitField, TextField, PasswordF
 from wtforms.validators import DataRequired
 
 class AddUser(FlaskForm):
-    user_id = StringField('Username:', validators=[DataRequired()])
     first_name = StringField('FirstName', validators= [DataRequired()])
     last_name = StringField('LastName', validators= [DataRequired()])
     email = StringField('Email', validators= [DataRequired()])
     address = StringField('Address', validators= [DataRequired()])
     gender = StringField('Gender', validators= [DataRequired()])
+    major_id= StringField('Gender', validators= [DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Save')
 
