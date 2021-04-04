f090ea0eb9aecbc7a3191b4da39e911f748fdbe8
YouAzo96 <youssef.azougagh96@gmail.com>
2021-04-04 20:59:51 +0000

    announcements system and routes changes, Also db models updated

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
 # New imports
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 import os
+
 
 # force loading of environment variables
 load_dotenv('.flaskenv')
@@ -15,6 +17,7 @@ USERNAME = os.environ.get('DATABASE_USERNAME')
 DB_NAME = os.environ.get('DATABASE_NAME')
 
 app = Flask(__name__)
+Bootstrap(app)
 app.config['SECRET_KEY'] = 'owlpool'
 
 # Add DB config
@@ -29,8 +32,17 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= True
 
 # Create database connection and associate it with the Flask application
 db = SQLAlchemy(app)
-
 # Add models
 from app import routes, models
+from app.models import User, Announcement
 
 db.create_all()
+
+admin_check = User.query.filter_by(email='youssef@owl.edu').first()
+if admin_check is None:
+    admin = User(user_id=1,first_name='Youssef',last_name="Youssef",address='NULL', user_type='admin',major_id=1,email='youssef@owl.edu',gender='male',active=True)
+    admin.set_password('password')
+    db.session.add(admin)
+    
+
+db.session.commit()
